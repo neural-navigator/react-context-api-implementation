5237aba049f2a523bb19e995aa0a1a336cb2e411
iamsatyapati <iamsatyapati@gmail.com>
2024-05-13 08:16:21 +0530

    context implementation completed

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './UserContext'
 
 class ComponentE extends Component {
+    static contextType = UserContext // this is the second way
   render() {
     return (
       <div>
+        ComponentE context {this.context}
         <ComponentF />
       </div>
     )
   }
 }
+
+ComponentE.contextType = UserContext // this is the first way 
 
 export default ComponentE
